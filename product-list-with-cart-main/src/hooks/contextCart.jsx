4c32addcce6c18549eddf80777ae8adbeba52657
Edilson Luciano <edison591@gmail.com>
2024-09-06 19:45:28 -0300

    adição project product-list-with-cart-main

---
+++ product-list-with-cart-main/src/hooks/contextCart.jsx
@@ -0,0 +1,26 @@
+import { createContext, useEffect, useState } from "react";
+import PropTypes from "prop-types";
+
+export const CartItemsContext = createContext();
+
+export function CartItemsProvider({ children }) {
+  const getCartItem = () => {
+    const savedCartItem = localStorage.getItem("cartItem");
+    return savedCartItem ? JSON.parse(savedCartItem) : [];
+  };
+  const [cartItem, setCartItem] = useState(getCartItem);
+
+  useEffect(() => {
+    localStorage.setItem("cartItem", JSON.stringify(cartItem));
+  }, [cartItem]);
+
+  return (
+    <CartItemsContext.Provider value={[cartItem, setCartItem]}>
+      {children}
+    </CartItemsContext.Provider>
+  );
+}
+
+CartItemsProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
